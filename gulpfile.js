d54f432a662c3f499e4cac6b81b5b143ee67f73d
Elias <martygoodfella@gmail.com>
2022-01-08 02:30:07 +0300

    saved

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,41 @@
 "use strict";
 
 const {src, dest} = require("gulp");
-
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
-const cssnano = require("gulp-cssnano");
-const imagemin = require("gulp-imagemin");
-const plumber = require("gulp-plumber");
+const removeComments = require('gulp-strip-css-comments');
 const rename = require("gulp-rename");
+const sass = require('gulp-sass');
+const cssnano = require("gulp-cssnano");
 const rigger = require("gulp-rigger");
-const sass = require("gulp-sass");
-const comments = require("gulp-strip-css-comments");
 const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
 const panini = require("panini");
+const browsersync = require("browser-sync").create();
 
 
-let path = {
+/* Paths */
+var path = {
     build: {
         html: "dist/",
         js: "dist/assets/js/",
         css: "dist/assets/css/",
-        img: "dist/assets/img/"
+        images: "dist/assets/img/"
     },
     src: {
         html: "src/*.html",
         js: "src/assets/js/*.js",
         css: "src/assets/sass/style.scss",
-        img: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
     },
     watch: {
         html: "src/**/*.html",
         js: "src/assets/js/**/*.js",
         css: "src/assets/sass/**/*.scss",
-        img: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
     },
     clean: "./dist"
 }
@@ -41,7 +43,18 @@ let path = {
 
 function html() {
     return src(path.src.html, {base: "src/"})
-        .pipe(dist(path.src.html))
+        .pipe(plumber())
+        .pipe(dest(path.build.html));
+}
+
+function css() {
+    return src(path.src.css, {base: "src/assets/sass/"})
+        .pipe(plumber())
+        .pipe(autoprefixer())
+        .pipe(cssbeautify())
+        .pipe(dest(path.build.css));
 }
 
 exports.html = html;
+exports.css = css;
+
