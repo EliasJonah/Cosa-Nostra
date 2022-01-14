87e81213a05425d103a6de47118bd275329059cd
EliasJonah <ilyas_19.04.20@mail.ru>
2022-01-14 18:47:02 +0300

    modalWindow is done

---
+++ dist/assets/js/app.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Modal
+    const modalTrigger = document.querySelector('[data-modal]'),
+          modal = document.querySelector('.modal__open'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+
+
+    function openModal() {
+        modal.style.display = 'block';
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+    }
+
+    modalTrigger.addEventListener('click', () => {
+        openModal();
+    })
+
+    modalCloseBtn.addEventListener('click', () => {
+        closeModal();
+    })
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    })
+
+    document.addEventListener('keydown', () => {
+        closeModal();
+    })
+})
